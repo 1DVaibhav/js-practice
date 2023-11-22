983f7b6df6831388e0284e27ec24bec343d1f23c
Vaibhav <vdd8010@gmail.com>
2023-11-22 20:03:51 +0530

    modified;

--- 02dataTypes.js
+++ 02dataTypes.js
@@ -6,9 +6,8 @@ string,
 symbol,
 number,
 object*/
-var myName = "Vaibhav";//var is accesible throghout the whole  program;
+var myName = "Vaibhav"; //var is accesible throghout the whole  program;
 myName = "v";
-let lastName="Deshmukh";//let is accesible only within scope where u have declared it;
-const number=7776035906;
-console.log(myName,lastName,number);
-
+let lastName = "Deshmukh"; //let is accesible only within scope where u have declared it;
+const number = 111115906;
+console.log(myName, lastName, number);
